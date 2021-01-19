474f8830123ddd3081df5a9850a0807e121b341e
ken025 <kennethmanuel250@gmail.com>
2021-01-19 13:01:52 -0500

    Added some articles to test layout

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 
 const Home = () => {
-    const [name, setName] = useState('ken')
-
-    const handleClick = (e) => {
-        setName("new");
-      }
+const [blogs, setBlogs] = useState([
+    { title: 'My Wew Recipes Website', body: 'Hey Queens...', author: 'Ken', id: 1 },
+    { title: 'Welcome party!', body: 'Bring your won air fried food...', author: 'Lumie', id: 2 },
+    { title: 'Top Air Fryer Tips', body: 'Here are some of the top tips...', author: 'Ken', id: 3 }
+  ])
 
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <h3> {blog.body} </h3>
+                    <p> Written by: {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,10 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
         <h1>AIR FRYER RECIPES BLOG</h1>
+        <div className="links">
         <a href='/'>Home</a>
         <a href='/create' id="nav-button">New Article</a>
+        </div>
         </nav>
      );
 }
