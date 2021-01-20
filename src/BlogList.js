84ecaf3f6ddb0b21f5250d3adec781c2ad31ac98
ken025 <kennethmanuel250@gmail.com>
2021-01-20 01:28:45 -0500

    added custom hook useFetch

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,18 +1,17 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
     //destructure props directly
     // const blogs = props.blogs
     // const title = props.title
 
     return ( 
         <div className="blog-list">
-            <h2 id="blog-list-title">{title}:</h2>
+            <h2 id="blog-list-title">All Blogs:</h2>
         {blogs.map(blog => (
             <div className="blog-preview" key={blog.id}>
                 <h1>{blog.title}</h1>
                 <h3> {blog.body} </h3>
                 <p> Written by: {blog.author}</p>
-                <button onClick={() => handleDelete(blog.id)}>delete blog</button>
-            </div>
+             </div>
         ))}
         </div>
      );
--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,17 @@
-import { useState, useEffect } from 'react'
-import BlogList from './BlogList'
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-      
-
-      const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-      }
-
-      useEffect(() =>{
-        fetch('  http://localhost:8000/blogs')
-            .then(resp => {
-               return resp.json();
-            })
-            .then(blogs => {
-                setBlogs(blogs)
-            })
-      }, []);
-
-    return ( 
-        <div className="home">
-        <blogs && BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-        </div>
-     );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
 export default Home;
